54d79f563591732cfb70129cd7f18d761294913a
Andrew Peterson <andrewpetersondev@gmail.com>
2025-01-13 00:50:39 -0600

    Refactor dashboard and improve database integration
    fetchRevenue and fetchLatestInvoices now have explicit types.
    
    Revised the dashboard structure and updated revenue chart logic. Enhanced database queries by replacing raw SQL with a Drizzle ORM implementation, improving readability and maintainability. Streamlined utility functions and adjusted formatting for better clarity.

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,52 +1,81 @@
 import { sql } from "@vercel/postgres";
+import { db } from "@/src/db/database";
 import {
   CustomerField,
   CustomersTableType,
+  Invoice,
   InvoiceForm,
   InvoicesTable,
+  LatestInvoice,
   LatestInvoiceRaw,
   Revenue,
 } from "@/src/lib/definitions";
 import { formatCurrency } from "@/src/lib/utils";
+import { customers, invoices, revenue } from "@/src/db/schema";
+import { desc, eq } from "drizzle-orm";
 
-export async function fetchRevenue() {
+export async function fetchRevenue(): Promise<Revenue[]> {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue>`SELECT *
-                                        FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data.rows;
+    const data: { revenue: number; month: string }[] = await db
+      .select()
+      .from(revenue);
+    return data;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch revenue data.");
   }
 }
 
-export async function fetchLatestInvoices() {
+export async function fetchLatestInvoices(): Promise<
+  {
+    amount: string;
+    email: string;
+    id: string;
+    image_url: string | null;
+    name: string;
+  }[]
+> {
   try {
-    const data = await sql<LatestInvoiceRaw>`
-            SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-            FROM invoices
-                     JOIN customers ON invoices.customer_id = customers.id
-            ORDER BY invoices.date DESC LIMIT 5`;
+    const data: {
+      amount: number;
+      email: string;
+      id: string;
+      image_url: string | null;
+      name: string;
+    }[] = await db
+      .select({
+        amount: invoices.amount,
+        name: customers.name,
+        image_url: customers.image_url,
+        email: customers.email,
+        id: invoices.id,
+      })
+      .from(invoices)
+      .innerJoin(customers, eq(invoices.customer_id, customers.id))
+      .orderBy(desc(invoices.date))
+      .limit(5);
 
-    const latestInvoices = data.rows.map((invoice) => ({
+    const latestInvoices: {
+      amount: string;
+      name: string;
+      email: string;
+      image_url: string | null;
+      id: string;
+    }[] = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
     return latestInvoices;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch the latest invoices.");
   }
 }
+
+fetchLatestInvoices().then((response) => {
+  console.log("response", response);
+});
 
 export async function fetchCardData() {
   try {
@@ -104,17 +133,12 @@ export async function fetchFilteredInvoices(
             FROM invoices
                      JOIN customers ON invoices.customer_id = customers.id
             WHERE customers.name ILIKE ${`%${query}%`}
-               OR
-                customers.email ILIKE ${`%${query}%`}
-               OR
-                invoices.amount::text ILIKE ${`%${query}%`}
-               OR
-                invoices.date::text ILIKE ${`%${query}%`}
-               OR
-                invoices.status ILIKE ${`%${query}%`}
+               OR customers.email ILIKE ${`%${query}%`}
+               OR invoices.amount::text ILIKE ${`%${query}%`}
+               OR invoices.date::text ILIKE ${`%${query}%`}
+               OR invoices.status ILIKE ${`%${query}%`}
             ORDER BY invoices.date DESC
-                LIMIT ${ITEMS_PER_PAGE}
-            OFFSET ${offset}
+            LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
         `;
 
     return invoices.rows;
@@ -130,14 +154,10 @@ export async function fetchInvoicesPages(query: string) {
                                 FROM invoices
                                          JOIN customers ON invoices.customer_id = customers.id
                                 WHERE customers.name ILIKE ${`%${query}%`}
-                                   OR
-                                    customers.email ILIKE ${`%${query}%`}
-                                   OR
-                                    invoices.amount::text ILIKE ${`%${query}%`}
-                                   OR
-                                    invoices.date::text ILIKE ${`%${query}%`}
-                                   OR
-                                    invoices.status ILIKE ${`%${query}%`}
+                                   OR customers.email ILIKE ${`%${query}%`}
+                                   OR invoices.amount::text ILIKE ${`%${query}%`}
+                                   OR invoices.date::text ILIKE ${`%${query}%`}
+                                   OR invoices.status ILIKE ${`%${query}%`}
         `;
 
     const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
@@ -154,14 +174,13 @@ export async function fetchInvoiceById(id: string) {
             SELECT invoices.id,
                    invoices.customer_id,
                    invoices.amount,
-                   invoices.status
+                   invoices.paymentStatus
             FROM invoices
             WHERE invoices.id = ${id};
         `;
 
     const invoice = data.rows.map((invoice) => ({
-      ...invoice,
-      // Convert amount from cents to dollars
+      ...invoice, // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
 
@@ -196,14 +215,17 @@ export async function fetchFilteredCustomers(query: string) {
                    customers.name,
                    customers.email,
                    customers.image_url,
-                   COUNT(invoices.id)                                                         AS total_invoices,
-                   SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-                   SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END)    AS total_paid
+                   COUNT(invoices.id)  AS total_invoices,
+                   SUM(CASE
+                           WHEN invoices.status = 'pending' THEN invoices.amount
+                           ELSE 0 END) AS total_pending,
+                   SUM(CASE
+                           WHEN invoices.status = 'paid' THEN invoices.amount
+                           ELSE 0 END) AS total_paid
             FROM customers
                      LEFT JOIN invoices ON customers.id = invoices.customer_id
             WHERE customers.name ILIKE ${`%${query}%`}
-               OR
-                customers.email ILIKE ${`%${query}%`}
+               OR customers.email ILIKE ${`%${query}%`}
             GROUP BY customers.id, customers.name, customers.email, customers.image_url
             ORDER BY customers.name ASC
         `;
