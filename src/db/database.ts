41c22f2792e397c4a2e098d682b83794e4e49faf
Andrew Peterson <andrewpetersondev@gmail.com>
2025-01-17 14:26:06 -0600

    Refactor database connection to use environment configuration
    Updated the database connection to consistently use DATABASE_URL for the connection string. Removed unused and commented-out code to improve clarity and maintainability.

--- src/db/database.ts
+++ src/db/database.ts
@@ -4,12 +4,14 @@ import pg from "pg";
 
 const { Pool } = pg;
 
+// connection from localhost to db container
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
 });
 
-// connection from localhost to db container
-export const db = drizzle({ client: pool });
-
 // connection from web-app to db container
-// export const db = drizzle(process.env.POSTGRES_URL!);
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL!,
+// });
+
+export const db = drizzle({ client: pool });
