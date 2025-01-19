6c39d0fc1222f60a6bb36e2475e56be234982ab3
Andrew Peterson <andrewpetersondev@gmail.com>
2025-01-19 10:32:54 -0600

    Remove server actions, add client error pages, refactor types.
    Removed server-side actions for invoices and replaced them with updated type definitions. Introduced client-side error handling pages for login and signup. Refactored and expanded types to better handle user and invoice schemas.

--- src/types/db.types.ts
+++
@@ -1,8 +0,0 @@
-// import { InferSelectModel } from "drizzle-orm";
-// import { users, customers, invoices, revenue } from "@/src/db/schema";
-
-// Raw table types (database-level)
-// export type User = InferSelectModel<typeof users>;
-// export type Customer = InferSelectModel<typeof customers>;
-// export type Invoice = InferSelectModel<typeof invoices>;
-// export type Revenue = InferSelectModel<typeof revenue>;
--- src/types/definitions.ts
+++ src/types/definitions.ts
@@ -5,7 +5,8 @@
 
 export type User = {
   id: string;
-  name: string;
+  // name: string;
+  username: string;
   email: string;
   password: string;
 };
@@ -87,3 +88,71 @@ export type InvoiceForm = {
   amount: number;
   status: "pending" | "paid" | null;
 };
+
+import { z } from "zod";
+
+export const SignupFormSchema = z.object({
+  username: z
+    .string()
+    .min(2, { message: "Username must be at least 2 characters long." })
+    .trim(),
+  email: z.string().email({ message: "Please enter a valid email." }).trim(),
+  password: z
+    .string()
+    .min(6, { message: "Be at least 6 characters long" })
+    .regex(/[a-zA-Z]/, { message: "Contain at least one letter." })
+    .regex(/[0-9]/, { message: "Contain at least one number." })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Contain at least one special character.",
+    })
+    .trim(),
+});
+
+export type SignupField = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type SignupFormState =
+  | {
+      errors?: {
+        name?: string[];
+        email?: string[];
+        password?: string[];
+      };
+      message?: string;
+    }
+  | undefined;
+
+/*
+export type SignupFormStateOptionB = {
+  errors?: {
+    username?: string[];
+    email?: string[];
+    password?: string[];
+  };
+};*/
+
+export type InvoiceState = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export const InvoiceFormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: "Invalid customer id",
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: "Amount must be greater than $0." }),
+  status: z.enum(["pending", "paid"], {
+    invalid_type_error: "Please select a status",
+  }),
+  date: z.string(),
+});
