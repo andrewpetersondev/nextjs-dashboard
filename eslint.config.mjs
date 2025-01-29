31f07c8fc252c817bd2b553a2f13c24b32577af2
Andrew Peterson <andrewpetersondev@gmail.com>
2025-01-28 23:25:04 -0600

    Refactor devcontainer setup, update service names, and configure Prettier and ESLint settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,21 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";  
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
   ...compat.extends(
     "next",
     "next/core-web-vitals",
