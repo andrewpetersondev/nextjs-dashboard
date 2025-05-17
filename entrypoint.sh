0857cb5db19e6be8b106111b47f16861e70ba5cf
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-05-17 05:49:35 -0500

    Add entrypoint script and enhance Docker Compose configuration; set container names and update entrypoint for database migrations and seeding

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,26 @@ log "Starting entrypoint script"
 
 # Load secrets into environment variables
 log "Loading secrets"
-export SESSION_SECRET=$(cat /run/secrets/session_secret)
 export POSTGRES_PASSWORD=$(cat /run/secrets/postgres_password)
+export POSTGRES_TESTDB_PASSWORD=$(cat /run/secrets/postgres_testdb_password)
+export POSTGRES_TESTDB_URL=$(cat /run/secrets/postgres_testdb_url)
 export POSTGRES_URL=$(cat /run/secrets/postgres_url)
+export SESSION_SECRET=$(cat /run/secrets/session_secret)
+
+log "Database Migrations"
+echo "Running database migrations..."
+pnpm drizzle-schema-update
+pnpm drizzle-schema-update-testDB
+
+log "Database Seeding"
+echo "Seeding database (if needed)..."
+
+log "Seeding Dev Database..."
+pnpm drizzle-seed
+
+log "Seeding Test Database..."
+pnpm drizzle-seed-testDB
 
 log "Entrypoint script completed, executing command: $@"
+echo "Starting Next.js app..."
 exec "$@"
