ee11207a87ced42cece828bd6c274aa6ebcda53f
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-05-19 04:03:07 -0500

    implemented checkbox and forgot password components

--- src/ui/auth/signup-form.tsx
+++ src/ui/auth/signup-form.tsx
@@ -2,7 +2,6 @@
 
 import { useActionState } from "react";
 import { signup } from "@/src/server-actions/users";
-import Link from "next/link";
 import { AtSymbolIcon, UserIcon, LockClosedIcon } from "@heroicons/react/24/outline";
 import type { FC } from "react";
 import { InputField } from "@/src/ui/auth/input-field";
@@ -10,6 +9,8 @@ import AuthSwitchLink from "./auth-switch-link";
 import Heading from "./heading";
 import { SocialLoginButton } from "./social-login-button";
 import { AuthSubmitButton } from "@/src/ui/auth/auth-submit-button";
+import { RememberMeCheckbox } from "./remember-me-checkbox";
+import { ForgotPasswordLink } from "./forgot-password-link";
 
 type SignupFormState = Readonly<{
 	errors?: {
@@ -74,48 +75,8 @@ export const SignupForm: FC = () => {
 						/>
 
 						<div className="flex items-center justify-between">
-							<div className="flex gap-3">
-								<div className="flex h-6 shrink-0 items-center">
-									<div className="group grid size-4 grid-cols-1">
-										<input
-											id="remember-me"
-											name="remember-me"
-											type="checkbox"
-											className="border-bg-accent bg-bg-accent text-bg-active focus:ring-bg-focus col-start-1 row-start-1 h-4 w-4 rounded"
-											aria-checked="false"
-											tabIndex={0}
-										/>
-										<svg
-											fill="none"
-											viewBox="0 0 14 14"
-											className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white opacity-0 group-has-[:checked]:opacity-100"
-											aria-hidden="true"
-										>
-											<title>Checkmark</title>
-											<path
-												d="M3 8L6 11L11 3.5"
-												strokeWidth={2}
-												strokeLinecap="round"
-												strokeLinejoin="round"
-											/>
-										</svg>
-									</div>
-								</div>
-								<label
-									htmlFor="remember-me"
-									className="text-text-primary block text-sm/6"
-								>
-									Remember me
-								</label>
-							</div>
-							<div className="text-sm/6">
-								<Link
-									href="/forgot-password"
-									className="text-text-secondary hover:text-text-hover font-semibold"
-								>
-									Forgot password?
-								</Link>
-							</div>
+							<RememberMeCheckbox />
+							<ForgotPasswordLink />
 						</div>
 
 						<div>
@@ -127,17 +88,19 @@ export const SignupForm: FC = () => {
 							</AuthSubmitButton>
 						</div>
 					</form>
+					{/* does this error div ever get used? */}
 					<div
 						className="flex h-8 items-end space-x-1"
 						aria-live="polite"
 						aria-atomic="true"
 					>
 						{state?.message && (
-							<p data-cy="signup-message-errors" className="text-bg-active">
+							<p data-cy="signup-message-errors" className="text-text-error">
 								{state.message}
 							</p>
 						)}
 					</div>
+					{/* does this error div ever get used? */}
 
 					<div>
 						<div className="relative mt-10">
