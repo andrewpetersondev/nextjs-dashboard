66570bd3ea46e81213456631c225ada3479d68f8
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-05-21 16:28:27 -0500

    Refactor DemoUser component to use form submission instead of button click for improved functionality and semantics.

--- src/ui/auth/demo-user.tsx
+++ src/ui/auth/demo-user.tsx
@@ -4,8 +4,10 @@ import { Button } from '@/ui/button'
 
 export default function DemoUser({ text }: { text: string }): JSX.Element {
   return (
-    <div>
-      <Button className='mt-2 bg-bg-primary text-text-primary ring-bg-accent hover:bg-bg-accent focus-visible:ring-bg-focus flex w-full items-center justify-center gap-3 rounded-md px-3 py-2 text-sm font-semibold ring-1 focus-visible:ring-2' type="button" onClick={demoUser}>{text}</Button>
-    </div>
+    <form action={async () => {
+      await demoUser();
+    }}>
+      <Button className='mt-2 bg-bg-primary text-text-primary ring-bg-accent hover:bg-bg-accent focus-visible:ring-bg-focus flex w-full items-center justify-center gap-3 rounded-md px-3 py-2 text-sm font-semibold ring-1 focus-visible:ring-2' type="submit" >{text}</Button>
+    </form>
   )
 }
