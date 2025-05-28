0268a0511a6d3bd787c868e374e5a69d4732f8e1
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-05-28 10:05:05 -0500

    Refactor Cypress configuration: clean up commented code and ensure baseUrl is set for e2e tests; remove obsolete client-component test file.

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,27 +4,19 @@ import { users } from "./src/db/schema";
 import { eq } from "drizzle-orm";
 
 export default defineConfig({
-	// env: {
-	// do i set postgres url here?
-	// fuck: "some_value",
-	// greeting: "fuck off"
-	// },
 	e2e: {
 		supportFile: "cypress/support/e2e.ts",
 		specPattern: "cypress/e2e/**/*.{cy,spec}.ts",
 		screenshotOnRunFailure: true,
 		video: true,
-		// baseUrl: "http://localhost:3000",
+		baseUrl: "http://localhost:3000",
 		setupNodeEvents(on, config) {
-			// console.log("env", process.env);
-			// console.log("config", config);
 			on("task", {
 
 				"logToConsole": (message) => {
 					console.log("log: ", message);
 					return null;
 				},
-
 				"db:insert": async (user) => {
 					const insertedUser = await db.insert(users).values(user).returning({ username: users.username, email: users.email, password: users.password });
 					return insertedUser ? "User created" : "User creation failed";
