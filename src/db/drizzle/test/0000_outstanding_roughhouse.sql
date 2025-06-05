866db60ff5a70967a65a2bc76891d120cea65189
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-06-04 19:13:20 -0500

    refactor invoice data structure and types; update image_url to imageUrl for consistency and introduce zod-alias for type imports

---
+++ src/db/drizzle/test/0000_outstanding_roughhouse.sql
@@ -0,0 +1,48 @@
+CREATE TYPE "public"."role" AS ENUM('guest', 'admin', 'user');--> statement-breakpoint
+CREATE TYPE "public"."status" AS ENUM('pending', 'paid');--> statement-breakpoint
+CREATE TABLE "customers" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" varchar(50) NOT NULL,
+	"email" varchar(50) NOT NULL,
+	"image_url" varchar(255) NOT NULL,
+	CONSTRAINT "customers_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "demo_user_counters" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"role" text NOT NULL,
+	"count" integer DEFAULT 0 NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "invoices" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"customer_id" uuid NOT NULL,
+	"amount" integer NOT NULL,
+	"status" "status" DEFAULT 'pending' NOT NULL,
+	"date" date NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "revenues" (
+	"month" varchar(4) NOT NULL,
+	"revenue" integer NOT NULL,
+	CONSTRAINT "revenues_month_unique" UNIQUE("month")
+);
+--> statement-breakpoint
+CREATE TABLE "sessions" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"token" text,
+	"expires_at" timestamp NOT NULL,
+	"user_id" uuid
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"username" varchar(50) NOT NULL,
+	"email" varchar(50) NOT NULL,
+	"role" "role" DEFAULT 'user' NOT NULL,
+	"password" varchar(255) NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "invoices" ADD CONSTRAINT "invoices_customer_id_customers_id_fk" FOREIGN KEY ("customer_id") REFERENCES "public"."customers"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
