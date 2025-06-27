c637cd3e7b418e212c2bc01029ecc11efae070f2
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-06-26 23:54:34 -0500

    feat(cypress): ordered alphabetically

--- cypress.d.ts
+++ cypress.d.ts
@@ -2,22 +2,25 @@
 
 import type { MountOptions, MountReturn } from "cypress/react";
 import type { ReactNode } from "react";
-import type { UserEntity } from "./src/lib/db/entities/user";
-import type { UserRole } from "./src/lib/definitions/enums";
 import type {
 	CreateUserInput,
 	LoginCredentials,
 	SignupUserInput,
 	UserCredentials,
 } from "./cypress/support/types";
+import type { UserEntity } from "./src/lib/db/entities/user";
+import type { UserRole } from "./src/lib/definitions/enums";
 
 declare global {
 	namespace Cypress {
 		interface Chainable {
-			mount(
-				component: ReactNode,
-				options?: Partial<MountOptions>,
-			): Chainable<MountReturn>;
+			createUser(user: CreateUserInput): Chainable<UserEntity>;
+
+			deleteUser(email: string): Chainable<UserEntity>;
+
+			ensureUserDeleted(email: string): Chainable<UserEntity | null>;
+
+			findUser(email: string): Chainable<UserEntity>;
 
 			login(user: LoginCredentials): Chainable<void>;
 
@@ -28,17 +31,14 @@ declare global {
 
 			loginSession(user: UserCredentials): Chainable<void>;
 
+			mount(
+				component: ReactNode,
+				options?: Partial<MountOptions>,
+			): Chainable<MountReturn>;
+
 			setMockSessionCookie(userId: string, role?: UserRole): Chainable<void>;
 
 			signup(user: SignupUserInput): Chainable<void>;
-
-			createUser(user: CreateUserInput): Chainable<UserEntity>;
-
-			deleteUser(email: string): Chainable<UserEntity>;
-
-			ensureUserDeleted(email: string): Chainable<UserEntity | null>;
-
-			findUser(email: string): Chainable<UserEntity>;
 
 			updateUser(
 				email: string,
