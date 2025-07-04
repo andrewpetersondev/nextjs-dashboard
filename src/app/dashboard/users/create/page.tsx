a1245ce96187443663b032dfa8a62de4faefab3b
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-07-03 19:14:38 -0500

    chore(page): remove biome ignore comments for linting rules

--- src/app/dashboard/users/create/page.tsx
+++ src/app/dashboard/users/create/page.tsx
@@ -2,18 +2,15 @@ import type { Metadata } from "next";
 import type { JSX } from "react";
 import { Breadcrumbs } from "@/src/ui/invoices/breadcrumbs.tsx";
 import { CreateUserForm } from "@/src/ui/users/create-user-form.tsx";
-import { CreateUserFormV2 } from "@/src/ui/users/create-user-form-v2.tsx";
+// import { CreateUserFormV2 } from "@/src/ui/users/create-user-form-v2.tsx";
 
-// biome-ignore lint/style/useComponentExportOnlyModules: ignore this rule
 export const metadata: Metadata = {
 	title: "Create User",
 };
 
 // force this page to be dynamic, so it doesn't get cached
-// biome-ignore lint/style/useComponentExportOnlyModules: Next.js requires this format
 export const dynamic = "force-dynamic";
 
-// biome-ignore lint/style/noDefaultExport: page and layout probably need to be default exports
 export default async function Page(): Promise<JSX.Element> {
 	return (
 		<main>
@@ -31,7 +28,7 @@ export default async function Page(): Promise<JSX.Element> {
 				]}
 			/>
 			<CreateUserForm />
-			<CreateUserFormV2 />
+			{/*<CreateUserFormV2 />*/}
 		</main>
 	);
 }
