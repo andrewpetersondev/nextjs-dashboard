0b7e746ec955cc7d8894ca698763ad44465828ae
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-07-07 08:06:49 -0500

    refactor(auth): improve login form structure and enhance accessibility

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import { type JSX, Suspense } from "react";
+import { Heading } from "@/src/ui/auth/heading";
 import { LoginForm } from "@/src/ui/auth/login-form";
 
 /**
@@ -9,9 +10,15 @@ import { LoginForm } from "@/src/ui/auth/login-form";
 export default function Page(): JSX.Element {
 	return (
 		<main className="h-full">
-			<Suspense fallback={<div>Loading ...</div>}>
-				<LoginForm />
-			</Suspense>
+			<div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+				<Heading text="Log in to your account" />
+				<Suspense fallback={<div>Loading ...</div>}>
+					<LoginForm />
+					{/*	LoginForm has */}
+					{/*	div has sub-div and AuthSwitchLink */}
+					{/*	sub-div has form, AuthServerMessage and LoginFormSocialSections*/}
+				</Suspense>
+			</div>
 		</main>
 	);
 }
--- src/ui/auth/login-form.tsx
+++ src/ui/auth/login-form.tsx
@@ -7,7 +7,6 @@ import { AuthServerMessage } from "@/src/ui/auth/auth-server-message";
 import { AuthSubmitButton } from "@/src/ui/auth/auth-submit-button";
 import { AuthSwitchLink } from "@/src/ui/auth/auth-switch-link";
 import { ForgotPasswordLink } from "@/src/ui/auth/forgot-password-link";
-import { Heading } from "@/src/ui/auth/heading";
 import { InputField } from "@/src/ui/auth/input-field";
 import { LoginFormSocialSection } from "@/src/ui/auth/login-form-social-section";
 import { RememberMeCheckbox } from "@/src/ui/auth/remember-me-checkbox";
@@ -33,67 +32,63 @@ export function LoginForm(): JSX.Element {
 	});
 
 	return (
-		<div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
-			<Heading text="Log in to your account" />
+		<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+			<div className="bg-bg-primary px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
+				<form action={action} className="space-y-6">
+					<InputField
+						autoComplete="email"
+						autoFocus={true}
+						dataCy="login-email-input"
+						describedById="login-email-errors"
+						error={state?.errors?.email}
+						icon={
+							<AtSymbolIcon className="text-text-accent pointer-events-none ml-2 h-[18px] w-[18px]" />
+						}
+						id="email"
+						label="Email address"
+						name="email"
+						placeholder="steve@jobs.com"
+						required={true}
+						type="email"
+					/>
+					<InputField
+						autoComplete="current-password"
+						dataCy="login-password-input"
+						describedById="login-password-errors"
+						error={state?.errors?.password}
+						icon={
+							<LockClosedIcon className="text-text-accent pointer-events-none ml-2 h-[18px] w-[18px]" />
+						}
+						id="password"
+						label="Password"
+						name="password"
+						placeholder="Enter your password"
+						required={true}
+						type="password"
+					/>
 
-			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
-				<div className="bg-bg-primary px-6 py-12 shadow-sm sm:rounded-lg sm:px-12">
-					<form action={action} className="space-y-6">
-						<InputField
-							autoComplete="email"
-							autoFocus={true}
-							dataCy="login-email-input"
-							describedById="login-email-errors"
-							error={state?.errors?.email}
-							icon={
-								<AtSymbolIcon className="text-text-accent pointer-events-none ml-2 h-[18px] w-[18px]" />
-							}
-							id="email"
-							label="Email address"
-							name="email"
-							placeholder="steve@jobs.com"
-							required={true}
-							type="email"
-						/>
-						<InputField
-							autoComplete="current-password"
-							dataCy="login-password-input"
-							describedById="login-password-errors"
-							error={state?.errors?.password}
-							icon={
-								<LockClosedIcon className="text-text-accent pointer-events-none ml-2 h-[18px] w-[18px]" />
-							}
-							id="password"
-							label="Password"
-							name="password"
-							placeholder="Enter your password"
-							required={true}
-							type="password"
-						/>
-
-						<FormInputWrapper>
-							<div className="flex items-center justify-between">
-								<RememberMeCheckbox />
-								<ForgotPasswordLink />
-							</div>
-						</FormInputWrapper>
-
-						<div>
-							<AuthSubmitButton data-cy="login-submit-button" pending={pending}>
-								Log In
-							</AuthSubmitButton>
+					<FormInputWrapper>
+						<div className="flex items-center justify-between">
+							<RememberMeCheckbox />
+							<ForgotPasswordLink />
 						</div>
-					</form>
+					</FormInputWrapper>
 
-					<AuthServerMessage message={state.message} />
-					<LoginFormSocialSection />
-				</div>
-				<AuthSwitchLink
-					href="/signup"
-					linkText="Sign up here"
-					prompt="Not a member?"
-				/>
+					<div>
+						<AuthSubmitButton data-cy="login-submit-button" pending={pending}>
+							Log In
+						</AuthSubmitButton>
+					</div>
+				</form>
+
+				<AuthServerMessage message={state.message} />
+				<LoginFormSocialSection />
 			</div>
+			<AuthSwitchLink
+				href="/signup"
+				linkText="Sign up here"
+				prompt="Not a member?"
+			/>
 		</div>
 	);
 }
