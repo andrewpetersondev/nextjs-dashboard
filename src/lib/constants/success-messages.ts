b5e7634259b86fb30f0f6223e2287aed2ad7400e
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-07-13 17:13:03 -0500

    feat(messages): add validation and success messages for invoices

--- src/lib/constants/success-messages.ts
+++ src/lib/constants/success-messages.ts
@@ -4,3 +4,14 @@ export const USER_SUCCESS_MESSAGES = {
   PARSE_SUCCESS: "User data parsed successfully.",
   UPDATE_SUCCESS: "User updated successfully.",
 };
+
+export const INVOICE_SUCCESS_MESSAGES = {
+  CREATE_SUCCESS: "Invoice created successfully.",
+  DELETE_SUCCESS: "Invoice deleted successfully.",
+  FETCH_ALL_SUCCESS: "All invoices fetched successfully.",
+  FETCH_FILTERED_SUCCESS: "Filtered invoices fetched successfully.",
+  FETCH_LATEST_SUCCESS: "Latest invoices fetched successfully.",
+  FETCH_PAGES_SUCCESS: "Total invoice pages fetched successfully.",
+  READ_SUCCESS: "Invoice fetched successfully.",
+  UPDATE_SUCCESS: "Invoice updated successfully.",
+};
