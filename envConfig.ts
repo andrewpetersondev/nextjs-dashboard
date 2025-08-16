432df453d648462e9b16b259a5b6aede2f739fc8
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-08-16 08:42:04 -0500

    update: bump dependencies and revise env config setup
    Upgraded `next`, `@types/node`, and `eslint-plugin-cypress` dependencies to latest minor versions. Refactored `envConfig.ts` to export a test-friendly async function.

--- envConfig.ts
+++ envConfig.ts
@@ -1,4 +1,10 @@
+// The below can be used in a Jest global setup file or similar for your testing set-up
 import { loadEnvConfig } from "@next/env";
 
-const projectDir = process.cwd();
-loadEnvConfig(projectDir);
+export default async () => {
+  const projectDir = process.cwd();
+  loadEnvConfig(projectDir);
+};
+
+// const projectDir = process.cwd();
+// loadEnvConfig(projectDir);
