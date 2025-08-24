a0f95d5c3b27d983b46ad3f256693b21f15487e4
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-08-24 06:40:11 -0500

    chore(scripts, config): update import paths to reflect new `scripts` directory structure; adjust related package.json scripts for consistency and alignment

--- scripts/drizzle-dev.config.ts
+++ scripts/drizzle-dev.config.ts
@@ -1,17 +1,15 @@
-import "./envConfig.ts";
+import "../envConfig.ts";
 import { defineConfig } from "drizzle-kit";
 
-console.log("drizzle-prod.config.ts ...");
-
-console.log(process.env.POSTGRES_URL_PRODDB);
+console.log("drizzle-dev.config.ts ...");
 
 export default defineConfig({
   casing: "snake_case",
   dbCredentials: {
     // biome-ignore lint/style/noNonNullAssertion: <good enough>
-    url: process.env.POSTGRES_URL_PRODDB!,
+    url: process.env.POSTGRES_URL!,
   },
   dialect: "postgresql",
-  out: "./drizzle/migrations/prod/",
+  out: "./drizzle/migrations/dev/",
   schema: "./src/server/db/schema.ts",
 });
--- scripts/drizzle-prod.config.ts
+++ scripts/drizzle-prod.config.ts
@@ -1,15 +1,17 @@
-import "./envConfig.ts";
+import "../envConfig.ts";
 import { defineConfig } from "drizzle-kit";
 
-console.log("drizzle-dev.config.ts ...");
+console.log("drizzle-prod.config.ts ...");
+
+console.log(process.env.POSTGRES_URL_PRODDB);
 
 export default defineConfig({
   casing: "snake_case",
   dbCredentials: {
     // biome-ignore lint/style/noNonNullAssertion: <good enough>
-    url: process.env.POSTGRES_URL!,
+    url: process.env.POSTGRES_URL_PRODDB!,
   },
   dialect: "postgresql",
-  out: "./drizzle/migrations/dev/",
+  out: "./drizzle/migrations/prod/",
   schema: "./src/server/db/schema.ts",
 });
