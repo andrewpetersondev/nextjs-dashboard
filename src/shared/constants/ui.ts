4b2f3bb41c33af55cc57bb45e6d0101be2a6ae47
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-08-23 21:42:49 -0500

    chore(styles, constants): add new font definitions and define constant for invoice pagination

--- src/shared/constants/ui.ts
+++ src/shared/constants/ui.ts
@@ -2,6 +2,8 @@ export const ITEMS_PER_PAGE = 10;
 
 export const ITEMS_PER_PAGE_USERS = 10;
 
+export const ITEMS_PER_PAGE_INVOICES = 10;
+
 export const DASHBOARD_TITLES = {
   ADMIN: "Admin Dashboard",
   GUEST: "Guest Dashboard",
--- src/ui/styles/fonts.ts
+++ src/ui/styles/fonts.ts
@@ -5,21 +5,25 @@ import { Doto, Merienda, Noto_Sans, Tektur } from "next/font/google";
 // notoSans is used for body text
 // merienda is used for font-eyegrab
 
+// biome-ignore lint/nursery/useExplicitType: <not shown in nextjs docs>
 export const notoSans = Noto_Sans({
   display: "swap",
   subsets: ["latin"],
 });
 
+// biome-ignore lint/nursery/useExplicitType: <not shown in nextjs docs>
 export const tektur = Tektur({
   display: "swap",
   subsets: ["latin"],
 });
 
+// biome-ignore lint/nursery/useExplicitType: <not shown in nextjs docs>
 export const merienda = Merienda({
   display: "swap",
   subsets: ["latin"],
 });
 
+// biome-ignore lint/nursery/useExplicitType: <not shown in nextjs docs>
 export const doto = Doto({
   display: "swap",
   subsets: ["latin"],
