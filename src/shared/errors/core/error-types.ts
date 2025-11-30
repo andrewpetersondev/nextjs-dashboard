5415c83f8a67124fd5bec4aaea87cff7563f63e3
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-11-30 02:20:27 -0600

    docs: add commentary on AppErrorLayer structure and suggest alternative layering approach

--- src/shared/errors/core/error-types.ts
+++ src/shared/errors/core/error-types.ts
@@ -1,5 +1,9 @@
 export type Severity = "ERROR" | "WARN" | "INFO";
 
+/**
+ * These layers do not seem to be well-structured. I thought the layers would be more like this: UI, API, DB, ACTION, SERVICE, REPO, DAL, etc.)
+ * - CORE, AUTH, VALIDATION,  DOMAIN, HTTP, seem to be the most out of place.
+ */
 export type AppErrorLayer =
   | "CORE"
   | "INFRA"
