40aa9355d8a83ca63d58b2ebb56d0175100864c0
Andrew Peterson <49768397+andrewpetersondev@users.noreply.github.com>
2025-12-11 14:19:58 -0600

    removed unused code

--- src/modules/auth/server/application/ports/password-hasher.port.ts
+++
@@ -1,8 +0,0 @@
-import "server-only";
-import type { PasswordHash } from "@/modules/auth/domain/password/password.types";
-
-export interface PasswordHasherPort {
-  compare(raw: string, hash: PasswordHash): Promise<boolean>;
-
-  hash(raw: string): Promise<PasswordHash>;
-}
